587c379972fa5d870590d7dd4ef4b87ebcb9da66
asdasewq <caojie.qu@gmail.com>
2026-05-19 06:02:58 -0400

    up

--- Decode/dwtdctsvd.sh
+++ Decode/dwtdctsvd.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-cd ../Baselines/blind_watermark
 
 source "$(conda info --base)/etc/profile.d/conda.sh"
 conda activate marknull
 
-python dwtDctSvd.py --path "$1"
+python ../Baselines/blind_watermark/dwtDctSvd.py --path "$1"
--- Decode/gs.sh
+++ Decode/gs.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
-cd ../Baselines/Gaussian-Shading
-
 source "$(conda info --base)/etc/profile.d/conda.sh"
 conda activate marknull
 
-python decode.py --path "$1"
+python ../Baselines/Gaussian-Shading/decode.py --path "$1"
